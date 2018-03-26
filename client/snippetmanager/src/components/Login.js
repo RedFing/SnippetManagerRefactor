e70b162a13fc41df70141ec0aea3b72e5c96c408
RedFing <muhamed.ahmatovic@gmail.com>
2018-03-26 18:31:39 +0200

    bug fix

--- client/snippetmanager/src/components/Login.js
+++ client/snippetmanager/src/components/Login.js
@@ -3,7 +3,6 @@ import React from 'react';
 import GitHubLogin from 'react-github-login';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { GITHUB_CLIENT_ID, GITHUB_STATE_STRING } from '../secrets';
 import { loginUser, loginUserFailed } from '../actions/authActions';
 
 import './Login.css';
@@ -37,7 +36,7 @@ class Login extends React.Component {
             onFailure={this.onLoginFailure}
             redirectUri={process.env.REACT_APP_GITHUB_REDIRECT_URI}
             scope="user:email, gist"
-            state={GITHUB_STATE_STRING}
+            state="random"
             className="btn btn-block btn-social btn-github"
             buttonText="Authenticate with github"
           />
